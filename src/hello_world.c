e09a50c12fa34cb1a28faa3d1d14c77fdcfb0243
NickEngmann <nickengmann_2@yahoo.com>
2016-06-03 23:40:08 -0400

    Timing
    Learned how to access the time variables

--- src/hello_world.c
+++ src/hello_world.c
@@ -2,16 +2,45 @@
 
 static Window *s_window;
 static TextLayer *s_text_layer;
+static char time_format[] = "00:00.";
+
+static void select_click_handler( ClickRecognizerRef recognizer, void *context){
+  text_layer_set_text(s_text_layer, "Select");
+}
+static void up_click_handler( ClickRecognizerRef recognizer, void *context){
+  text_layer_set_text(s_text_layer, "Up");
+}
+static void down_click_handler( ClickRecognizerRef recognizer, void *context){
+  text_layer_set_text(s_text_layer, "Down");
+}
+static void click_config_provider(void *context){
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+void tick_handler(struct tm *t, TimeUnits units_changed){
+  //with this I now have the active time at all times. I can check the time_format variable against the global variable that I intend on saving
+  if(!clock_is_24h_style()){
+    strftime(time_format, sizeof(time_format), "%I:%M", t);
+  }
+  else{
+    strftime(time_format, sizeof(time_format), "%H:%M", t);
+  }
+  text_layer_set_text(s_text_layer, time_format);
+}
 
 static void init(void) {
 	// Create a window and get information about the window
 	s_window = window_create();
+  window_set_click_config_provider(s_window, click_config_provider);
   Layer *window_layer = window_get_root_layer(s_window);
   GRect bounds = layer_get_bounds(window_layer);
-	
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);	
   // Create a text layer and set the text
 	s_text_layer = text_layer_create(bounds);
-	text_layer_set_text(s_text_layer, "Hi, I'm a Pebble!");
+	//Old Hello World Text
+  //text_layer_set_text(s_text_layer, "Hi, Making Sure I'm Running!");
   
   // Set the font and text alignment
 	text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
@@ -36,10 +65,15 @@ static void deinit(void) {
 	
 	// Destroy the window
 	window_destroy(s_window);
+  //destroy tick handler
+  tick_timer_service_unsubscribe();
 }
 
 int main(void) {
 	init();
+  //above event loop is create
 	app_event_loop();
+  //app event loop is what happens in the middle
+  //deinit is what happens when the user closes the app
 	deinit();
 }
